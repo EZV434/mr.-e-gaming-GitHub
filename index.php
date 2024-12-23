6083da3028605eb853ace4d4c82fc2eb20a66cd4
EZV434 <ezehivictor2@gmail.com>
2024-12-23 11:20:05 +0100

    Update index.php

--- index.php
+++ index.php
@@ -1,23 +1,26 @@
-<?php
-
-if
- ($_SERVER["REQUEST_METHOD"] == "POST") {
-
-    // Sanitize user input
-
-    $username = htmlspecialchars($_POST['username']);
-
-    echo "Hello, " . $username;
-
-}
-
-?>
 <!DOCTYPE html>
 <html>
- <head>
-  <title>Welcome  Page</title>
- </head>
- <body>
-  
- </body>
+<head>
+	<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+	<title>Login Form</title>
+</head>
+<body>
+	<div class="d-flex justify-content-center align-items-center vh-100">
+<div class="mb-3">
+	<form class="shadow w-450 p-3"
+	      method="POST"
+	      action="login-form.html">
+  <?php
+if
+ ($_SERVER["REQUEST_METHOD"] == "POST") {
+    // Sanitize user input
+    $username = htmlspecialchars($_POST['username']);
+    echo "Hello, " . $username;
+}
+?>
+		<button class="btn btn-primary">Logout</button>
+</div>
+</form>
+</div>
+</body>
 </html>
